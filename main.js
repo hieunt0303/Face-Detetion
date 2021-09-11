7ec6d7f0438d7bfc8de42b459f4fb2284a2dfcbf
Nguyễn Trung Hiếu <66076345+hieunt0303@users.noreply.github.com>
2021-09-11 20:17:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const video=document.getElementById('videoElm')
 
 const loadFaceAPI=async () =>{
-    await faceapi.nets.faceLandmark68Net.loadFromUri('/FaceDetetion/models'),
-    await faceapi.nets.faceRecognitionNet.loadFromUri('/FaceDetetion/models'),
-    await faceapi.nets.tinyFaceDetector.loadFromUri('/FaceDetetion/models'),
-    await faceapi.nets.faceExpressionNet.loadFromUri('/FaceDetetion/models')
+    await faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    await faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+    await faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+    await faceapi.nets.faceExpressionNet.loadFromUri('models')
 }
 function getCameraStream(){
 if(navigator.mediaDevices.getUserMedia){
